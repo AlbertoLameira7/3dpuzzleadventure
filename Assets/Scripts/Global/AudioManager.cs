28e935026a32e6ca96a3b154f5322de4c78a4f52
Alberto Lameira <lameira.alberto94@gmail.com>
2022-05-17 16:39:52 +0100

    Audio Manager, added background and steps sound clips

---
+++ Assets/Scripts/Global/AudioManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private AudioSource _background;
+    [SerializeField] private AudioSource _steps;
+
+    void Awake()
+    {
+        
+    }
+}
